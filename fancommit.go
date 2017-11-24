c5505ce171d055b90db1b743a19ad1af7fb8a039
cjlibra <cjlibra@qq.com>
2017-11-24 10:52:44 +0800

    应该没有动过，只是测试url

--- fancommit.go
+++ fancommit.go
@@ -10,8 +10,8 @@ import (
 	
 )
 const (
-   url = "192.168.1.8:1200"
-  // url = "127.0.0.1:1200"
+   //url = "192.168.1.8:1200"
+   url = "127.0.0.1:1200"
    manhandle = "aaeecc07010b010000000014"
    autohandle = "aaeecc07010b020000000015"
  //  lasting  = time.Second*60
